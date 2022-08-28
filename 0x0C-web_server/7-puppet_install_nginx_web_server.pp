adadadb9771da18fd1b5cd495763d8730abc3586
METC <metc_ck1@hotmail.com>
2022-08-28 11:10:33 -0700

    7-puppet_install_nginx_web_server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Install and configure ann nginx server with Puppet
+
+package { 'nginx':
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+	content	=> 'Hello World',
+}
+
+file_line { 'redirect permanently 301':
+	ensure 	=> present,
+	path	=> '/etc/nginx/sites-available/default',
+	after	=> 'server_name_;',
+	line	=> 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service {'nginx':
+	ensure	=> running,
+	require	=> Package['nginx'],
+{
