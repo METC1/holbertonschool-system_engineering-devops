8ea8711001e6c8e5063585dc95a1e6284d31a256
METC1 <metc_ck1@hotmail.com>
2022-09-28 15:27:41 -0700

    fix alignment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Puppet script to fix Wordpress settings
-exec { 'Fix_wordpresss':
-    command    =>'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php';
-    provider   =>shell,
-    }
+exec { 'fix_wordpresss':
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+    provider => shell,
+}
