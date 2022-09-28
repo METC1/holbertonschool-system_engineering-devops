5d42d07592148b56aeb21e3d21986e984b0a351b
METC1 <metc_ck1@hotmail.com>
2022-09-28 15:11:16 -0700

    fix second typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Puppet script to fix Wordpress settings
 exec { 'Fix_wordpresss':
-	command	=>	"sed '/sphpp/php/g'/var/www/html/wp-settings.php";
+	command	=>	"sed -i '/sphpp/php/g'/var/www/html/wp-settings.php";
 	provider=>	shell,
 	}
